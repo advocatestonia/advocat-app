677fdfc8870abda004355d9e2b247f7ea03267f4
Dmitri Sulga <ai.place@MacBook-Pro-Dmitri.local>
2026-04-10 14:34:28 +0300

    Deploy: honest pricing text, fixed Google logo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "719096132" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "976838732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
