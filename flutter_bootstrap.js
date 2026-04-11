e6cbe4984e8431d9724cf5ee0465fe242d9bff2a
Dmitri Sulga <ai.place@MacBook-Pro-Dmitri.local>
2026-04-11 13:16:21 +0300

    Deploy: AI Claude Code mode — full upgrade

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4258004240" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "345611530" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
