7faa605e43a3310808c79f257c096deef143ba08
Dmitri Sulga <ai.place@MacBook-Pro-Dmitri.local>
2026-04-11 08:53:40 +0300

    Deploy: alimony €318.62 + 6 mobile fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1770039725" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4063197557" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
