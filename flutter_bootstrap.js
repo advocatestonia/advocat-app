2286a52166a8dfd5ef79d482cab6e5578410b034
advocat-deploy <deploy@advocat.ee>
2026-04-30 16:51:34 +0300

    deploy: 9f45c84 (bigger shield favicon)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2406301065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3214584238" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
