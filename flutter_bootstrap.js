12c4b2b4afecb481a9db34c147b5162bb23ec500
advocat-deploy <deploy@advocat.ee>
2026-05-02 20:27:13 +0300

    deploy: 555a2f0 (VS mobile fix — stack vertical, smaller shield, simplified counter)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1336121211" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4041597220" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
