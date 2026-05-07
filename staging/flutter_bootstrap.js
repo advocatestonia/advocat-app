a8fea9e199954b89d3654bcd0a9d0a657e657344
Dmitri Sulga <aiplacest@gmail.com>
2026-05-07 13:06:50 +0300

    canary: staging build from 0dc11e5

--- staging/flutter_bootstrap.js
+++ staging/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2195976448" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2670599008" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
